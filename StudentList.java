67fba2bee7e5d6342a80c1b40f37ed8a7fcfb142
ishmam <mdishmamzahin@gmail.com>
2023-08-17 20:41:55 +0600

    added a constants class

--- StudentList.java
+++ StudentList.java
@@ -8,7 +8,7 @@ public class StudentList {
 
 	public static void readFile(){
 		try{
-			BufferedReader fileIn = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+			BufferedReader fileIn = new BufferedReader(new InputStreamReader(new FileInputStream(Constants.fileName)));
 			line = fileIn.readLine();
 			names = line.split(", ");
 		}
@@ -19,15 +19,15 @@ public class StudentList {
 
 	public static void writeFile(){
 		try{
-			BufferedWriter fileOut = new BufferedWriter(new FileWriter("students.txt", false));
+			BufferedWriter fileOut = new BufferedWriter(new FileWriter(Constants.fileName, false));
 			fileOut.write(line+", "+newStudentName+'\n');
 
 			Date date = new Date();
-			String dateFormat = "dd/mm/yyyy-hh:mm:ss a";
+			String dateFormat = Constants.dateFormat;
 			DateFormat dateFormatTemp = new SimpleDateFormat(dateFormat);
 			String dateline= dateFormatTemp.format(date);
 
-			fileOut.write("List last updated on "+dateline);
+			fileOut.write(Constants.outputMassegeOne+" "+dateline);
 			fileOut.close();
 		}
 		catch (Exception e){
@@ -40,40 +40,40 @@ public class StudentList {
 //		Check arguments
 		if(args.length!=1){
 			//terminate
-			System.out.println("Invalid Arguments!");
+			System.out.println(Constants.invalidOutputMassege);
 			return;
 		}
 		else{
 			readFile();
 
 			if(args[0].equals("a")) {
-				System.out.println("Loading data ...");
+				System.out.println(Constants.getOutputMassegeTwo);
 
 					for(String name : names) { System.out.println(name); }
 
-				System.out.println("Data Loaded.");
+				System.out.println(Constants.outputMassegeThree);
 			}
 			else if(args[0].equals("r"))
 			{
-				System.out.println("Loading data ...");
+				System.out.println(Constants.getOutputMassegeTwo);
 
 					Random random = new Random();
 					int randomNum = random.nextInt(names.length);
 					System.out.println(names[randomNum]);
 
-				System.out.println("Data Loaded.");
+				System.out.println(Constants.outputMassegeThree);
 			}
 			else if(args[0].contains("+")){
-				System.out.println("Loading data ...");
+				System.out.println(Constants.getOutputMassegeTwo);
 
 					newStudentName = args[0].substring(1);
 					writeFile();
 
-				System.out.println("Data Loaded.");
+				System.out.println(Constants.outputMassegeThree);
 			}
 			else if(args[0].contains("?"))
 			{
-				System.out.println("Loading data ...");
+				System.out.println(Constants.getOutputMassegeTwo);
 
 					String studentName = args[0].substring(1);
 					for(String name: names){
@@ -83,15 +83,15 @@ public class StudentList {
 						}
 					}
 
-				System.out.println("Data Loaded.");
+				System.out.println(Constants.outputMassegeThree);
 			}
 			else if(args[0].contains("c"))
 			{
-				System.out.println("Loading data ...");
+				System.out.println(Constants.getOutputMassegeTwo);
 
 				System.out.println(names.length+" word(s) found");
 
-				System.out.println("Data Loaded.");
+				System.out.println(Constants.outputMassegeThree);
 			}
 			else{
 				System.out.println("Constant invalid!");
